ec19090dfc60cdd287982737e1284f69b6767470
Makiri-Code <manuel.makiri@gmail.com>
2025-11-30 21:27:00 +0100

    Reverted the metadata images to a single Image

--- app/shortlet/[propertyslug]/page.js
+++ app/shortlet/[propertyslug]/page.js
@@ -6,7 +6,7 @@ import PropertyPhotoSection from "@/app/_components/PropertyPhotosSection";
 import PropertySavedIcon from "@/app/_components/ProperySavedIcon";
 import { getProperty } from "@/app/_lib/data-services"
 
-export async function generateMetadata({ params }) {
+export async function generateMetadata({params}){
     const pageParams = await params
     const property = await getProperty(pageParams.propertyslug);
     return {
@@ -15,11 +15,12 @@ export async function generateMetadata({ params }) {
         openGraph: {
             title: property?.property_name,
             description: property.description,
-            images: property.images.map((img) => ({
-                url: `https://app.xpacy.com/src/upload/properties/${img}`,
-                width: 1200,
-                height: 630
-            }))
+            images: [
+                {url: `https://app.xpacy.com/src/upload/properties/${property?.images.at(0)}`,
+                    width: 1200,
+                    height: 630
+                }
+            ]
         }
     }
 }
