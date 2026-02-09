c132d7b10c02e98b9bc7224a874825d067e92676
Makiri-Code <manuel.makiri@gmail.com>
2026-02-09 18:54:52 +0100

    Adjusted add owner component

--- app/_components/AdminPropertyOwnersList.js
+++ app/_components/AdminPropertyOwnersList.js
@@ -27,6 +27,19 @@ export default function AdminPropertyOwnersList({ owners }) {
 
     return (
         <DashboardGridItem title={"Property Owners List"}>
+            <div className="self-end">
+                <Modal>
+                    <Modal.Open>
+                        <button className="flex items-center justify-center cursor-pointer gap-2 px-4 py-3 bg-primary rounded-lg font-mono font-medium text-white">
+                            <span className="text-2xl"><HiOutlineUserAdd /></span>
+                            <span>Add New Owner</span>
+                        </button>
+                    </Modal.Open>
+                    <Modal.Window>
+                        <AddNewOwnerForm/>
+                    </Modal.Window>
+                </Modal>
+            </div>
             <TableHead tableCol="grid-cols-[1fr_1.5fr_1fr_0.5fr]" headingsArray={tableHeadings} />
             {owners?.map(({ first_name, last_name, email, display_picture, phone, id }) => (
                 <div className="grid grid-cols-[1fr_1.5fr_1fr_0.5fr] text-neutrals-900 text-sm font-mono border-b border-primary-100  ">
@@ -47,19 +60,7 @@ export default function AdminPropertyOwnersList({ owners }) {
                     </div>
                 </div>
             ))}
-            <div className="self-end">
-                <Modal>
-                    <Modal.Open>
-                        <button className="flex items-center justify-center cursor-pointer gap-2 px-4 py-3 bg-primary rounded-lg font-mono font-medium text-white">
-                            <span className="text-2xl"><HiOutlineUserAdd /></span>
-                            <span>Add New Owner</span>
-                        </button>
-                    </Modal.Open>
-                    <Modal.Window>
-                        <AddNewOwnerForm/>
-                    </Modal.Window>
-                </Modal>
-            </div>
+            
         </DashboardGridItem>
     )
 }
